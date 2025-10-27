8b8ca02d620b9a86a3629386676372d3de7f4398
lee <dieasel@naver.com>
2025-10-27 14:37:27 +0900

    2025-10-27 commit: Add code

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,15 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App.js';
-import { Provider } from 'react-redux';
-import { store } from './app/store.js';
+// src/index.js
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
+import App from "./App";
+import "./index.css";
+import "./App.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <Provider store={ store }>
-  <React.StrictMode>
+  <Router>
     <App />
-  </React.StrictMode>
-  </Provider>
+  </Router>
 );
