ecd7b4da9a90c279944b8d8f3d30b474b21a665e
doyoon0 <doyoon.2109@gmail.com>
2025-10-31 11:44:00 +0900

    signup-login-merge success!!!!!

--- backend/src/main/java/com/springboot/ecommerce_fullstack_app/dto/KakaoReadyResponse.java
+++
@@ -1,9 +0,0 @@
-package com.springboot.ecommerce_fullstack_app.dto;
-
-import lombok.Data;
-
-@Data
-public class KakaoReadyResponse {
-    private String tid; // 결제 고유 번호
-    private String next_redirect_pc_url;  //QR CODE ADDRESS
-}
